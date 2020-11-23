dd69dc3b678aa3e15d53c4c02ce5cbef29087ffd
dabels2s <abels.daniel@googlemail.com>
2020-11-23 12:23:53 +0100

    Übung 3 Test

--- src/org/hbrs/se/ws20/uebung3/persistence/PersistenceStrategyStream.java
+++ src/org/hbrs/se/ws20/uebung3/persistence/PersistenceStrategyStream.java
@@ -1,10 +1,11 @@
 package org.hbrs.se.ws20.uebung3.persistence;
 
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
+
+import java.io.*;
 import java.util.List;
 
 public class PersistenceStrategyStream<Member> implements PersistenceStrategy<Member> {
+    public final static String LOCATION = "/Users//Dokumente Local/Software Projects/SE/userstories18.ser";
     @Override
     public void openConnection() throws PersistenceException {
 
@@ -20,7 +21,25 @@ public class PersistenceStrategyStream<Member> implements PersistenceStrategy<Me
      * Method for saving a list of Member-objects to a disk (HDD)
      */
     public void save(List<Member> member) throws PersistenceException  {
-
+        List<Member> list = null;
+        ObjectOutputStream oos = null;
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream( PersistenceStrategyStream.LOCATION );
+            oos = new ObjectOutputStream(fos);
+            oos.writeObject( list );
+            System.out.println(  list.size() + " Member wurden erfolgreich gespeichert!");
+        }
+        catch (IOException e) {
+            // Koennte man ausgeben für interne Debugs: e.printStackTrace();
+            // Chain of Responsibility: Hochtragen der Exception in Richtung Ausgabe (UI)
+            // Uebergabe in ein lesbares Format fuer den Benutzer
+            throw new PersistenceException();
+        }
+        finally {
+            if (oos != null) try { oos.close(); } catch (IOException e) {}
+            if (fos != null) try { fos.close(); } catch (IOException e) {}
+        }
     }
 
     @Override
@@ -46,6 +65,35 @@ public class PersistenceStrategyStream<Member> implements PersistenceStrategy<Me
         // return newListe
 
         // and finally close the streams (guess where this could be...?)
-        return null;
+        List<Member> list = null;
+        ObjectInputStream ois = null;
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream( PersistenceStrategyStream.LOCATION );
+            ois = new ObjectInputStream(fis);
+
+            // Auslesen der Liste
+            Object obj = ois.readObject();
+            if (obj instanceof List<?>) {
+                list = (List) obj;
+
+            }
+            System.out.println("Es wurden " + list.size() + " User Stories erfolgreich reingeladen!");
+            return list;
+        }
+        catch (IOException e) {
+            // Sup-Optimal, da Exeception in Form eines unlesbaren Stake-Traces ausgegeben wird
+            e.printStackTrace();
+        }
+        catch (ClassNotFoundException e) {
+            // Verbesserung, aber Chain of Responsbility nicht erfuellt, da UI nicht
+            // benachrichtigt wird unter Umstaenden. Verbesserung: siehe Methoden store!
+            System.out.println("FEHLER: Liste konnte nicht extrahiert werden (ClassNotFound)!");
+        }
+        finally {
+            if (ois != null) try { ois.close(); } catch (IOException e) {}
+            if (fis != null) try { fis.close(); } catch (IOException e) {}
+        }
+        return list;
     }
 }
